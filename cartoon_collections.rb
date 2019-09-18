0a5c96c4efcd2cf8c0586fa71e2ae57bd013868c
Maynard Hartman <mjhartman@brighthouse.com>
2019-09-18 16:07:15 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,13 +1,31 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array) # code an argument here
+ array.each_with_index { |name, index|
+  puts "#{index + 1} #{name}"
+ }
+ return array
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(array) # code an argument here
   # Your code here
+  ret_array = array.map { |name|
+      name.capitalize
+      name + "!"
+  }
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(calls) # code an argument here
   # Your code here
+  calls.each { |call|
+    result = NIL
+    if call.size > 4
+      result = TRUE
+    else if 
+      call.size < 4
+      result = FALSE
+    end
+  }
+  puts result
+  return result
 end
 
 def find_the_cheese# code an argument here
