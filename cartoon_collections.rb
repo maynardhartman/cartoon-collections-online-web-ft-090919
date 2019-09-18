f109e8bc8ac1a90de218c3a198e9541a8d6915a9
Maynard Hartman <mjhartman@brighthouse.com>
2019-09-18 16:08:59 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,7 +2,6 @@ def roll_call_dwarves(array) # code an argument here
  array.each_with_index { |name, index|
   puts "#{index + 1} #{name}"
  }
- return array
 end
 
 def summon_captain_planet(array) # code an argument here
